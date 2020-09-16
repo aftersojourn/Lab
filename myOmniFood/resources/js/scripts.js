acc29cbafb21400133c6fd7436a23df18f02df39
edison2rinen <edison2rinen@hotmail.com>
2020-09-17 08:35:55 +0900

    i think 1.5s is better

--- myOmniFood/resources/js/scripts.js
+++ myOmniFood/resources/js/scripts.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     $('.js--scroll-to-plans').click(function() {
         $('html, body').animate({
             scrollTop: $('.js--section-plans').offset().top
-        }, 1000)
+        }, 1500)
 
     })
 
