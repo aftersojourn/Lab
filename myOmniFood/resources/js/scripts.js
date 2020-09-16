63b23e36ab6e8b9f3fc0cc16b9cd8c7f77cd6039
edison2rinen <edison2rinen@hotmail.com>
2020-09-17 08:46:53 +0900

    scroll to start

--- myOmniFood/resources/js/scripts.js
+++ myOmniFood/resources/js/scripts.js
@@ -14,20 +14,18 @@ $(document).ready(function() {
             offset: '70px'   
           })
 
-    // Hero Text //
+    // Scroll to Buttons //
     $('.js--scroll-to-plans').click(function() {
         $('html, body').animate({
             scrollTop: $('.js--section-plans').offset().top
         }, 1500)
-
     })
 
-    /* Sample
-    $("button").click(function() {
-        $('html,body').animate({
-            scrollTop: $(".second").offset().top},
-            'slow');
-    });
-    */
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({
+            scrollTop: $('.js--section-feature').offset().top
+        }, 1500)
+    })
 
+    
 });
