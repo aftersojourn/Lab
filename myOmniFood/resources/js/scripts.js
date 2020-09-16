85b9ac4d6519bafa2774ee5b3d861dfa825f766e
edison2rinen <edison2rinen@hotmail.com>
2020-09-17 08:34:04 +0900

    scroll to js--section-plans with 1s speed

--- myOmniFood/resources/js/scripts.js
+++ myOmniFood/resources/js/scripts.js
@@ -1,15 +1,33 @@
 
 $(document).ready(function() {
 
-      $('.js--section-feature').waypoint(function(direction) {
+    // Sticky Nav //
+    $('.js--section-feature').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass();
         }
     }, {
-            /* 25% of the current section and can also be changed to pixel. */
-           /* offset: '25%'   */
+        /* 25% of the current section and can also be changed to pixel. */
+        /* offset: '25%'   */
             offset: '70px'   
           })
+
+    // Hero Text //
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000)
+
+    })
+
+    /* Sample
+    $("button").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".second").offset().top},
+            'slow');
+    });
+    */
+
 });
