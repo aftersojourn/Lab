03e3708b247e20204a13e61235753877be52af9c
edison2rinen <edison2rinen@hotmail.com>
2020-09-16 14:33:57 +0900

    removed samples

--- myOmniFood/resources/js/scripts.js
+++ myOmniFood/resources/js/scripts.js
@@ -9,27 +9,3 @@ $(document).ready(function() {
         }
     })
 });
-
- /* Just A Test */
-    /*
-    $('h1').click(function() {
-        $(this).css('background-color', '#ff0000')
-    });
-    */
-
-    /* mod this
-    var waypoints = $('#handler-first').waypoint(function(direction) {
-    notify(this.element.id + ' hit 25% from top of window') 
-    }, {
-    offset: '25%'
-    })
-    */
-/*
-    $('js--section-feature').waypoint(function(direction) {
-        if (direction == down) {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    })
-*/
